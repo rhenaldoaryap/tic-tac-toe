b8360d9af8d233e7abc34f7cc8e661ee297cf1aa
Rhenaldo Arya <rhenaldoaryap@gmail.com>
2022-02-02 19:27:53 +0700

    tracking selected fields on the game board

--- script/game.js
+++ script/game.js
@@ -24,7 +24,24 @@ function selectGameField(event) {
     return;
   }
 
-  event.target.textContent = players[activePlayer].symbol;
-  event.target.classList.add("disabled");
+  const selectedField = event.target;
+  const selectedColumn = selectedField.dataset.col - 1;
+  const selectedRow = selectedField.dataset.row - 1;
+  // - 1 for making an array start from zero like that used to be.
+
+  if (gameData[selectedRow][selectedColumn] > 0) {
+    alert("Please select an empty field");
+    return;
+  }
+
+  selectedField.textContent = players[activePlayer].symbol;
+  selectedField.classList.add("disabled");
+
+  gameData[selectedRow][selectedColumn] = activePlayer + 1;
+  console.log(gameData);
+  //first square bracket for main array, there are 3 rows, that is the main array.
+  //second square bracket for array inner an array, basically second square bracket for handling which column that selected by user.
+  // activePlayer + 1 because we want to player 1 equal to 1 and player 2 equal to 2. 0 for not any player in this game because by default we declare activePlayer = 0, see app.js
+
   switchPlayer();
 }
